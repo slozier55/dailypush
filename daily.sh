a3fefda5396bda317384a4d431baa044cc94496c
Carlos Toxtli <ctoxtli@gmail.com>
2016-03-14 22:52:40 +0000

    New changes

---
+++ daily.sh
@@ -0,0 +1,5 @@
+echo -e "." >> README.md
+git add *
+git commit -m 'New changes'
+git push origin master
+
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-# Remember replace toxtli with your username and dailypush with your repository/application name!
+# Remember replace USER with your username and REPO with your repository/application name!
 git init
-curl -u 'toxtli' https://api.github.com/user/repos -d '{"name":"dailypush"}'
-git remote add origin https://github.com/toxtli/dailypush.git
+curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
+git remote add origin https://USER:PASS@github.com/USER/REPO.git
 echo "." > README.md
 git add *
+git config --global user.email "EMAIL"
+git config --global user.name "YOUR NAME"
 git commit -m 'First commit.'
 git push origin master
