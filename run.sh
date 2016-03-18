0cdb9497dfa70446e6816659162cb6f3a25c5bac
Carlos Toxtli <ctoxtli@gmail.com>
2016-03-18 03:50:50 +0000

    New changes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Remember replace USER with your username and REPO with your repository/application name!
 git init
-curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
+curl -u 'USER:PASS' https://api.github.com/user/repos -d '{"name":"REPO"}'
 git remote add origin https://USER:PASS@github.com/USER/REPO.git
 echo "." > README.md
 git add *
