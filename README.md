fbe2986b30278daae6689aeef0ff26a597587924
Carlos Toxtli <ctoxtli@gmail.com>
2016-03-18 14:52:01 +0000

    New changes

--- README.md
+++ README.md
@@ -3952,3 +3952,4 @@ Every dot below this line is the number of times that the tool has been executed
 .
 .
 .
+.
--- daily.sh
+++ daily.sh
@@ -1,9 +1,9 @@
 cd ~/dailypush/
-for i in {1..10}
+for i in {1..5}
 do
 echo "." >> README.md
 git add *
 git commit -m 'New changes'
 git push origin master
-sleep 2
+sleep 5
 done
